84f01eef29a8f197e18edb535d3d710bb7d21804
unnu3590 <unnu3590@ISS147D8-10.scdsb.pri>
2015-09-11 10:24:46 -0400

    momentum is complete

--- src/momentum.java
+++ src/momentum.java
@@ -1,20 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author unnu3590
- */
+import hsa.*;
+        
 public class momentum {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+       Console c= new Console();
+
+        double mass, vel, mome;
+        
+        c.print("Enter object's mass: ");
+                mass=c.readDouble();
+                
+        c.print("Enter object's velocity: ");
+                vel=c.readDouble();
+        
+        mome= vel*mass;
+        
+        c.print("The momentum is: "+mome);
+                
     }
     
 }
