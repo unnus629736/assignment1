efab243ade5bae6c6b81865491d2321c1b4bb3af
unnu3590 <unnu3590@ISS147D8-10.scdsb.pri>
2015-09-14 10:39:29 -0400

    line

--- src/makeLine.java
+++ src/makeLine.java
@@ -1,20 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import hsa.*;
+import TurtleGraphics.*;
 
-/**
- *
- * @author unnu3590
- */
 public class makeLine {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Console c = new Console();
+        SketchPadWindow sp= new SketchPadWindow(400,400);
+        
+        StandardPen p =new StandardPen();
+        
+        p.up();
+        
     }
     
 }
