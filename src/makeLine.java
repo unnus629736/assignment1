ffd9394196d0d60215c48e62f0859af81a9cadc7
unnu3590 <unnu3590@ISS171D8-04.scdsb.pri>
2015-09-14 12:59:17 -0400

    string method

--- src/makeLine.java
+++ src/makeLine.java
@@ -6,11 +6,26 @@ public class makeLine {
     
     public static void main(String[] args) {
         Console c = new Console();
+        double x1=0, x2=0, y1=0, y2=0;
+        c.print("Enter x-1 -->");
+          x1= c.readDouble();
+        c.print("Enter x-1 -->");
+        y1= c.readDouble();
+        c.print("Enter y-1 -->");
+        x2= c.readDouble();
+        c.print("Enter x-2 -->");
+        y2= c.readDouble();
+        c.print("Enter y-2 -->");
+        c.close();
+        
         SketchPadWindow sp= new SketchPadWindow(400,400);
         
-        StandardPen p =new StandardPen();
+        StandardPen p =new StandardPen(sp);
         
         p.up();
+        p.move(x1-200,y1-200);
+        p.down();
+        p.move(y2-200, y1-200);
         
     }
     
